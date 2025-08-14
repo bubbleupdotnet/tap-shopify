b4a0674887469a8508b532f6bf6589431dbf471d
Joe Gibson <mrjoefgibson@gmail.com>
2025-08-14 14:41:10 +0000

    Adding balance transactions options

--- tap_shopify/streams/__init__.py
+++ tap_shopify/streams/__init__.py
@@ -18,3 +18,5 @@ import tap_shopify.streams.fulfillment_orders
 import tap_shopify.streams.order_shipping_lines
 import tap_shopify.streams.payouts
 import tap_shopify.streams.balance_transactions
+import tap_shopify.streams.balance_transactions_transfers
+import tap_shopify.streams.balance_transactions_nontransfers
